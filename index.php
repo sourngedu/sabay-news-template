6e79b8e66e379b977a05267f339ab99120ff7f81
Hok Vicheka <53561018+hvicheka@users.noreply.github.com>
2020-01-17 02:19:48 +0700

    update responsive

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <div class="container-fluid top-width">
         <div class="container top-logo">
                 <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 logo">
                         <img class="logo-img" src="./img/logo.png" alt="">
                     </div>
-                    <div class="col-lg-9 col-md-9 col-sm-9">
+                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 top-ads">
                         <img class="main-ads" src="./img/top-main-ads.jpg" alt="">
                     </div>
                 </div>
@@ -16,36 +16,9 @@
     </div>
     <div class="container-fluid top-menu">
         <div class="container menu">
-            <div class="row">
-                <span class="search-toggle">
-                    <img src="https://cdn2.iconfinder.com/data/icons/ui-line-6/32/hamburger_menu-512.png" alt="">
-                </span>
-                <!-- <span class="close-toggle">
-                    <img src="https://www.heartyhomes.com.au/wp-content/uploads/intense-cache/icons/plugin/font-awesome/times.svg" alt="">
-                </span> -->
-                <ul class="menu-bar">
-                        <li>
-                            <a href="#"><i class="fa fa-home"></i></a>
-                            <a href="#">កម្សាន្ត</a>
-                            <a href="#">បច្ចេកវិទ្យា</a>
-                            <a href="#">ជីវិត និងសង្គម</a>
-                            <a href="#">កីឡា</a>
-                        </li>
-                </ul>
-            </div>    
+            <?php include('nav-bar.php'); ?>
         </div>
     </div>
 </body>
-
-    <!-- <script>
-        $(document).ready(function(){
-            $('.menu-bar').hide();
-            $('.search-toggle').click(function(){
-                // alert('Hello');
-                $('.menu-bar').show();
-                $('.menu-bar').slideToggle();
-            });
-
-        });
-    </script> -->
+    
 </html>
---
+++ nav-bar.php
@@ -0,0 +1,14 @@
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <ul class="menu-bar">
+         <li><a class="active" href="#"><i class="fa fa-home"></i>&nbsp; ទំព័រដើម</a></li>
+        <li><a href="#">កម្សាន្ត</a></li>
+        <li><a href="#">បច្ចេកវិទ្យា</a></li>
+        <li><a href="#">ជីវិតនិងសង្គម</a></li>
+        <li><a href="#">កីឡា</a></li>
+    </ul>
+    <span class="brand-name">Sabay News</span>
+</nav>
