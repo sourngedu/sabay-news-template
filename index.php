5067ad815023fc3568cd06f1c33f10278eec2c6a
Hok Vicheka <53561018+hvicheka@users.noreply.github.com>
2020-01-16 22:42:38 +0700

    update

--- index.php
+++ index.php
@@ -15,17 +15,37 @@
         </div>
     </div>
     <div class="container-fluid top-menu">
-        <div class="container">        
-            <ul>
-                <li>
-                    <a href="#" class="active">ទំព័រដើម</a>
-                    <a href="#">កម្សាន្ត</a>
-                    <a href="#">បច្ចេកវិទ្យា</a>
-                    <a href="#">ជីវិត និងសង្គម</a>
-                    <a href="#">កីឡា</a>
-                </li>
-            </ul>
+        <div class="container menu">
+            <div class="row">
+                <span class="search-toggle">
+                    <img src="https://cdn2.iconfinder.com/data/icons/ui-line-6/32/hamburger_menu-512.png" alt="">
+                </span>
+                <!-- <span class="close-toggle">
+                    <img src="https://www.heartyhomes.com.au/wp-content/uploads/intense-cache/icons/plugin/font-awesome/times.svg" alt="">
+                </span> -->
+                <ul class="menu-bar">
+                        <li>
+                            <a href="#"><i class="fa fa-home"></i></a>
+                            <a href="#">កម្សាន្ត</a>
+                            <a href="#">បច្ចេកវិទ្យា</a>
+                            <a href="#">ជីវិត និងសង្គម</a>
+                            <a href="#">កីឡា</a>
+                        </li>
+                </ul>
+            </div>    
         </div>
     </div>
 </body>
+
+    <!-- <script>
+        $(document).ready(function(){
+            $('.menu-bar').hide();
+            $('.search-toggle').click(function(){
+                // alert('Hello');
+                $('.menu-bar').show();
+                $('.menu-bar').slideToggle();
+            });
+
+        });
+    </script> -->
 </html>
