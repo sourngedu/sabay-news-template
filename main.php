b1cf275aa985599241f3f125137464fbcbde55f0
Hok Vicheka <53561018+hvicheka@users.noreply.github.com>
2020-01-19 23:05:15 +0700

    update index and main

---
+++ main.php
@@ -0,0 +1,64 @@
+<!-- Slide Area  -->
+<div class="row slide-area">
+    <div class="col-md-9">
+        <div class="row">
+            <div class="col-md-7 slide slide1">
+                <div class="slide-category cat-sport">កីឡា</div>
+                <img src="./img/soccer-slide1.jpg" alt=""/>
+                <div class="slide-hover"></div>
+                <div class="slide-discription">
+                    <h4 class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</h4>
+                    <span class="slide-title-discription">មូលហេតុដាច់ដោយឡែកមួយ</span>
+                </div>
+                <div class="slide-upload-date">
+                    សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
+                </div>
+            </div>
+            <div class="col-md-5 slide slide2">
+                <div class="slide-category cat-sport">កីឡា</div>
+                <img src="./img/island-slide3.jpg" alt=""/>
+                <div class="slide-hover"></div>
+                <div class="slide-discription">
+                    <h4 class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</h4>
+                    <span class="slide-title-discription">មូលហេតុដាច់ដោយឡែកមួយ</span>
+                </div>
+                <div class="slide-upload-date">
+                    សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
+                </div>
+            </div>
+            <div class="col-md-7 slide slide3">
+                <div class="slide-category cat-entertainment">កីឡា</div>
+                <img src="./img/war-slide2.jpg" alt=""/>
+                <div class="slide-hover"></div>
+                <div class="slide-discription">
+                    <h4 class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</h4>
+                    <span class="slide-title-discription">មូលហេតុដាច់ដោយឡែកមួយ</span>
+                </div>
+                <div class="slide-upload-date">
+                    សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
+                </div>
+            </div>
+            <div class="col-md-5 slide slide4">
+                <div class="slide-category cat-entertainment">កីឡា</div>
+                <img src="./img/slide-ads2.jpg" alt=""/>
+                <div class="slide-hover"></div>
+                <div class="slide-discription">
+                    <h4 class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</h4>
+                    <span class="slide-title-discription">មូលហេតុដាច់ដោយឡែកមួយ</span>
+                </div>
+                <div class="slide-upload-date">
+                    សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 ads-area">
+        <div class="ads ads1">
+            <img src="./img/slide-ads1.jpg" alt="">
+        </div>
+        <div class="ads ads2">
+                <img src="./img/slide-ads2.jpg" alt="">
+        </div>
+    </div>
+</div>
+<!-- End Slide Area -->
