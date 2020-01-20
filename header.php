1d75e5c6457522b7341c341ed6ac73c05024165b
Hok Vicheka <53561018+hvicheka@users.noreply.github.com>
2020-01-20 17:01:40 +0700

    arrage folder

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <link rel="SHORTCUT ICON" href="./img/favicon.ico" type="image/x-icon" />
+    <link rel="SHORTCUT ICON" href="./assets/img/favicon.ico" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css?family=Angkor|Bayon|Chenla|Content|Dangrek|Koulen|Siemreap&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="script.js"></script>
+    <script src="./assets/js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="./style.css" />
-    <link rel="stylesheet" type="text/css" href="./style-responsive.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/style-responsive.css" />
--- main.php
+++ main.php
@@ -2,20 +2,20 @@
 <div class="row slide-area">
     <div class="col-md-9">
         <div class="row">
-            <div class="col-md-7 slide slide1">
-                <div class="slide-category cate-sport">កីឡា</div>
+            <a href="#" target="_blank" class="col-md-7 slide slide1">
+                <div class="slide-category slide-cate-sport">កីឡា</div>
                 <img src="./img/soccer-slide1.jpg" alt=""/>
                 <div class="slide-hover"></div>
                 <div class="slide-discription">
-                    <a href="#" class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</a>
+                    <h4 class="main-slide-title">មក​ដឹង​ប្រវត្តិ​ Red War មុន​ Liverpool និង​ Man Utd ប៉ះ​គ្នា​ថ្ងៃ​អាទិត្យ​នេះ​</h4>
                     <span class="slide-title-discription">មូលហេតុដាច់ដោយឡែកមួយ</span>
                 </div>
                 <div class="slide-upload-date">
                     សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
                 </div>
-            </div>
-            <div class="col-md-5 slide slide2">
-                <div class="slide-category cate-sport">កីឡា</div>
+            </a>
+            <a href="#" target="_blank" class="col-md-5 slide slide2">
+                <div class="slide-category slide-cate-sport">កីឡា</div>
                 <img src="./img/island-slide3.jpg" alt=""/>
                 <div class="slide-hover"></div>
                 <div class="slide-discription">
@@ -25,9 +25,9 @@
                 <div class="slide-upload-date">
                     សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
                 </div>
-            </div>
-            <div class="col-md-7 slide slide3">
-                <div class="slide-category cate-entertainment">កីឡា</div>
+            </a>
+            <a href="#" target="_blank" class="col-md-7 slide slide3">
+                <div class="slide-category slide-cate-entertainment">កីឡា</div>
                 <img src="./img/war-slide2.jpg" alt=""/>
                 <div class="slide-hover"></div>
                 <div class="slide-discription">
@@ -37,9 +37,9 @@
                 <div class="slide-upload-date">
                     សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
                 </div>
-            </div>
-            <div class="col-md-5 slide slide4">
-                <div class="slide-category cate-entertainment">កីឡា</div>
+            </a>
+            <a href="#" target="_blank" class="col-md-5 slide slide4">
+                <div class="slide-category slide-cate-entertainment">កីឡា</div>
                 <img src="./img/slide-ads2.jpg" alt=""/>
                 <div class="slide-hover"></div>
                 <div class="slide-discription">
@@ -49,7 +49,7 @@
                 <div class="slide-upload-date">
                     សុក្រ,​១៨ មករា ២០២០ ៨ៈ០០ ព្រឹក
                 </div>
-            </div>
+            </a>
         </div>
     </div>
     <div class="col-md-3 ads-area">
@@ -71,6 +71,45 @@
         <div class="category cate-video">
             <a href="#" class="category-item">វីដេអូ &gt;</a>
         </div>
-        <div class="vertical-line cate-video"></div>
+        <div class="vertical-line vl-video"></div>
+    </div>
+</div>
+<br>
+<!-- កម្សាន្ត -->
+<div class="category-area">
+    <div class="cate-title">
+        <div class="category cate-entertainment">
+            <a href="#" class="category-item">កម្សាន្ត &gt;</a>
         </div>
+        <div class="vertical-line vl-entertainment"></div>
+    </div>
+</div>
+<br>
+<!-- បច្ចេកវិទ្យា -->
+<div class="category-area">
+    <div class="cate-title">
+        <div class="category cate-technology">
+            <a href="#" class="category-item">បច្ចេកវិទ្យា &gt;</a>
+        </div>
+        <div class="vertical-line vl-technology"></div>
+    </div>
+</div>
+<!-- ជីវិត និងសង្គម -->
+<div class="category-area">
+    <div class="cate-title">
+        <div class="category cate-life">
+            <a href="#" class="category-item">ជីវិត និងសង្គម &gt;</a>
+        </div>
+        <div class="vertical-line vl-life"></div>
+    </div>
+</div>
+<br>
+<!-- កីឡា -->
+<div class="category-area">
+    <div class="cate-title">
+        <div class="category cate-sport">
+            <a href="#" class="category-item">​ កីឡា​ &gt;</a>
+        </div>
+        <div class="vertical-line vl-sport"></div>
+    </div>
 </div>
